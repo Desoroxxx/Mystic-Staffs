89eb7478a4f8df4556a659d4b165266e3a065116
Desoroxxx <desoroxxx@gmail.com>
2024-01-16 18:50:36 +0100

    Cleanup

--- src/main/java/dev/redstudio/mysticstaffs/config/MysticStaffsConfig.java
+++ src/main/java/dev/redstudio/mysticstaffs/config/MysticStaffsConfig.java
@@ -6,7 +6,8 @@ import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import static dev.redstudio.mysticstaffs.ProjectConstants.*;
+import static dev.redstudio.mysticstaffs.ProjectConstants.ID;
+import static dev.redstudio.mysticstaffs.ProjectConstants.NAME;
 
 @SuppressWarnings("CanBeFinal")
 @Config(modid = ID, name = NAME)
