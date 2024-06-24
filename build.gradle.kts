1d00ebf1c59761ac5ee10366b335ac59ec2d03cf
Desoroxxx <desoroxxx@gmail.com>
2024-06-24 15:19:17 +0200

    Update everything Gradle touching

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ import org.jetbrains.gradle.ext.Gradle
 import org.jetbrains.gradle.ext.runConfigurations
 
 plugins {
-    id("com.gtnewhorizons.retrofuturagradle") version "1.3.26"
+    id("com.gtnewhorizons.retrofuturagradle") version "1.4.0"
     id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.8"
     id("com.github.gmazzo.buildconfig") version "5.3.5"
-    id("io.freefair.lombok") version "8.4"
+    id("io.freefair.lombok") version "8.6"
 }
 
 group = "dev.redstudio"
@@ -19,7 +19,6 @@ minecraft {
     mcVersion = "1.12.2"
     username = "Desoroxxx"
     extraRunJvmArguments = listOf("-Dforge.logging.console.level=debug")
-    javaToolchain.get().vendor.set(JvmVendorSpec.ADOPTIUM)
 }
 
 repositories {
