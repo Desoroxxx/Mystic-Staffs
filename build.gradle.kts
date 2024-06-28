a2913e2857d583f7ba65be86001f888061f72a04
Desoroxxx <desoroxxx@gmail.com>
2024-06-28 21:23:00 +0200

    Version Bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "dev.redstudio"
-version = "1.2" // Versioning must follow Ragnarök versioning convention: https://shor.cz/ragnarok_versioning_convention
+version = "2.0" // Versioning must follow Ragnarök versioning convention: https://github.com/Red-Studio-Ragnarok/Commons/blob/main/Ragnar%C3%B6k%20Versioning%20Convention.md
 
 val name = "Mystic Staffs"
 val id = "mysticstaffs"
